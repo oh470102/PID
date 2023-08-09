0d4029ee346656fcb9009039e253c9f7c76b9309
oh470102 <oh470102@gmail.com>
2023-08-09 13:53:07 +0900

    tested new best pid

--- test_PID.py
+++ test_PID.py
@@ -5,7 +5,8 @@ import numpy as np
 Test any PID coefficient!
 '''
 
-PID = np.array([-70.86045582, 123.30184012, -60.15679048, 109.16208843, -29.05265625, 33.42436623])
+PID = np.array([-92.46397335, 150,         -79.52151719 , 79.39413694, -42.29593014,
+  43.68005843])
 # PID = np.array([-57, 103, -34,  87, -13,  16])
 
 env = cppid.CartPoleEnv(render_mode='human', control_mode='pid2')
